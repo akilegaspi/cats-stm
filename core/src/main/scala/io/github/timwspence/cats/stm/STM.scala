2ddde03b621bf6114d74b9ce6fdf263b8bbdc5b1
Tim Spence <timothywspence@gmail.com>
2020-09-21 10:01:55 +0100

    WIP

--- core/src/main/scala/io/github/timwspence/cats/stm/STM.scala
+++ core/src/main/scala/io/github/timwspence/cats/stm/STM.scala
@@ -2,15 +2,13 @@ package io.github.timwspence.cats.stm
 
 import java.util.concurrent.atomic.AtomicLong
 
-import cats.effect.Async
+import cats.implicits._
 import cats.{Monad, Monoid, MonoidK}
-
-import io.github.timwspence.cats.stm.STM.internal._
+import cats.effect.Concurrent
+import cats.effect.concurrent.Deferred
 
 import scala.annotation.tailrec
-import scala.collection.immutable.Queue
-import scala.collection.mutable.{Map => MMap}
-import scala.compat.java8.FunctionConverters._
+// import scala.compat.java8.FunctionConverters._
 
 /**
   * Monad representing transactions involving one or more
@@ -18,7 +16,7 @@ import scala.compat.java8.FunctionConverters._
   *
   * This design was inspired by [Beautiful Concurrency](https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/beautiful.pdf) and informed by ZIO
   * which has a common origin in that paper via the [stm package](http://hackage.haskell.org/package/stm).
- */
+  */
 sealed abstract class STM[+A] {
 
   /**
@@ -34,11 +32,17 @@ sealed abstract class STM[+A] {
   /**
     * Try an alternative `STM` action if this one retries.
     */
-  final def orElse(fallback: STM[A]): STM[A] = OrElse(this, fallback)
+  final def orElse[B >: A](fallback: STM[B]): STM[B] = OrElse(this, fallback)
+
+  /**
+    * Run transaction atomically
+    */
+  def atomically[F[_]: Concurrent, B >: A]: F[B] = STM.atomically[F](this)
 
 }
 
 object STM {
+  import internal._
 
   /**
     * Commit the `STM` action as an `IO` action. The mutable
@@ -81,6 +85,7 @@ object STM {
     */
   val unit: STM[Unit] = pure(())
 
+  //TODO can we make an ApplicativeError/MonadError here?
   implicit val stmMonad: Monad[STM] with MonoidK[STM] = new Monad[STM] with MonoidK[STM] {
     override def flatMap[A, B](fa: STM[A])(f: A => STM[B]): STM[B] = fa.flatMap(f)
 
@@ -101,46 +106,197 @@ object STM {
     }
 
   final class AtomicallyPartiallyApplied[F[_]] {
-    def apply[A](stm: STM[A])(implicit F: Async[F]): F[A] = ???
 
-    private def run[A](stm: STM[A]): TResult[A] = {
-      var conts: List[Any => STM[Any]] = Nil
-      var state: Map[TVar, Int] = Map.empty
+    def apply[A](stm: STM[A])(implicit F: Concurrent[F]): F[A] = {
+      //TODO retry other txns
+      val (r, log) = run(stm)
+      r match {
+        case TSuccess(res) =>
+          var commit = false
+          STM.synchronized {
+            if (!log.isDirty) {
+              commit = true
+              log.commit()
+            }
+          }
+          if (commit) F.pure(res) else apply(stm)
+        case TFailure(e) => F.raiseError(e)
+        case TRetry =>
+          for {
+            defer <- Deferred[F, Either[Throwable, A]]
+            _     <- F.pure("TODO: instantiate retry thread")
+            e     <- defer.get
+            res   <- e.fold(F.raiseError(_), F.pure(_))
+          } yield res
+      }
 
-      @tailrec
-      def go(stm: STM[Any]): TResult[Any] = stm match {
-        case Pure(a) => {
-          if (conts.isEmpty) TSuccess(a) else {
+    }
+
+    case class RetryFiber[A](stm: STM[A], defer: Deferred[F, Either[Throwable, A]]) {
+      def run(implicit F: Concurrent[F]): F[Unit] = {
+        val (r, log) = STM.run(stm)
+        r match {
+          case TSuccess(res) =>
+            var commit = false
+            STM.synchronized {
+              if (!log.isDirty) {
+                commit = true
+                log.commit()
+              }
+            }
+            if (commit) defer.complete(Right(res)) else run
+          case TFailure(e) => defer.complete(Left(e))
+          case TRetry      => F.unit
+        }
+      }
+    }
+
+  }
+
+  private[stm] def run[A](stm: STM[A]): (TResult[A], TLog) = {
+    var conts: List[Cont]                             = Nil
+    var fallbacks: List[(STM[Any], TLog, List[Cont])] = Nil
+    var log: TLog                                     = TLog.empty
+
+    @tailrec
+    def go(stm: STM[Any]): TResult[Any] =
+      stm match {
+        case Pure(a) =>
+          if (conts.isEmpty) TSuccess(a)
+          else {
             val f = conts.head
             conts = conts.tail
             go(f(a))
           }
-        }
         case Bind(stm, f) =>
           conts = f :: conts
           go(stm)
-        case Get(tvar) => ??? //TODO update state
-        case Modify(tvar, f) => ??? //TODO update state
-        case OrElse(attempt, fallback) => ??? //TODO possibly need another set of stacks to handle this?
-        case Abort(erro) => TFailure(error)
-        case Retry => TRetry
-
+        case Get(tvar)       => go(Pure(log.get(tvar)))
+        case Modify(tvar, f) => go(Pure(log.modify(tvar, f)))
+        case OrElse(attempt, fallback) =>
+          fallbacks = (fallback, log.snapshot(), conts) :: fallbacks
+          go(attempt)
+        case Abort(error) =>
+          //TODO do we use fallback on error?
+          if (fallbacks.isEmpty) TFailure(error)
+          else {
+            val (fb, lg, cts) = fallbacks.head
+            log = lg
+            conts = cts
+            fallbacks = fallbacks.tail
+            go(fb)
+          }
+        case Retry =>
+          if (fallbacks.isEmpty) TRetry
+          else {
+            val (fb, lg, cts) = fallbacks.head
+            log = lg
+            conts = cts
+            fallbacks = fallbacks.tail
+            go(fb)
+          }
       }
 
-      go(stm)
+    go(stm).asInstanceOf[TResult[A]] -> log
+  }
+
+  private[stm] object internal {
+
+    val IdGen = new AtomicLong()
+
+    case class TLog(private var map: Map[TVar[Any], TLogEntry]) {
+
+      def values: Iterable[TLogEntry] = map.values
+
+      def get(tvar: TVar[Any]): Any =
+        if (map.contains(tvar))
+          map(tvar).unsafeGet[Any]
+        else
+          map = map + (tvar -> TLogEntry(tvar, tvar.value))
+
+      def modify(tvar: TVar[Any], f: Any => Any): Unit = {
+        val current = get(tvar)
+        val entry   = map(tvar)
+        entry.unsafeSet(f(current))
+      }
+
+      def isDirty: Boolean = values.exists(_.isDirty)
+
+      def snapshot(): TLog = TLog(map)
+
+      def commit(): Unit = values.foreach(_.commit())
+
+    }
+
+    object TLog {
+      def empty: TLog = TLog(Map.empty)
+    }
+
+    type Cont = Any => STM[Any]
+
+    type Pending = () => Unit
+
+    type TxId = Long
+
+    case class Txn(id: TxId, pending: Pending, tvs: Set[ETVar])
+
+    //TODO can we just make this Any now that we're covariant or use existential types?
+    // Existential TVar
+    abstract class ETVar {
+      type Repr
+      val tv: TVar[Repr]
+    }
+
+    object ETVar {
+      def apply[A](t: TVar[A]): ETVar =
+        new ETVar {
+          override type Repr = A
+          override val tv = t
+        }
+    }
+
+    //TODO can/should we just make this any?
+    abstract class TLogEntry {
+      type Repr
+      var current: Repr
+      val initial: Repr
+      val tvar: TVar[Repr]
+
+      def unsafeGet[A]: A = current.asInstanceOf[A]
+
+      def unsafeSet[A](a: A): Unit = current = a.asInstanceOf[Repr]
+
+      def commit(): Unit = tvar.value = current
+
+      def reset(): Unit = current = initial
+
+      def isDirty: Boolean = initial != tvar.value.asInstanceOf[Repr]
+
+    }
+
+    object TLogEntry {
+
+      def apply[A](tvar0: TVar[A], current0: A): TLogEntry =
+        new TLogEntry {
+          override type Repr = A
+          override var current: A    = current0
+          override val initial: A    = tvar0.value.asInstanceOf[A]
+          override val tvar: TVar[A] = tvar0
+        }
+
     }
 
   }
 
 }
 
-private[stm] final case class Pure[+A](a: A) extends STM[A]
-private[stm] final case class Bind[+A, B](stm: STM[B], f: B => STM[A]) extends STM[A]
-private[stm] final case class Get[+A](tvar: TVar[A]) extends STM[A]
-private[stm] final case class Modify[+A](tvar: TVar[A], f: A => A) extends STM[Unit]
-private[stm] final case class OrElse[+A](attempt: STM[A], fallback: STM[A]) extends STM[A]
-private[stm] final case class Abort(error: Throwable) extends STM[Nothing]
-private[stm] final case object Retry extends STM[Nothing]
+final private[stm] case class Pure[+A](a: A)                                extends STM[A]
+final private[stm] case class Bind[+A, B](stm: STM[B], f: B => STM[A])      extends STM[A]
+final private[stm] case class Get[+A](tvar: TVar[A])                        extends STM[A]
+final private[stm] case class Modify[+A, B](tvar: TVar[B], f: B => A)       extends STM[Unit]
+final private[stm] case class OrElse[+A](attempt: STM[A], fallback: STM[A]) extends STM[A]
+final private[stm] case class Abort(error: Throwable)                       extends STM[Nothing]
+final private[stm] case object Retry                                        extends STM[Nothing]
 
 sealed trait TResult[+A]                    extends Product with Serializable
 final case class TSuccess[A](value: A)      extends TResult[A]
@@ -340,104 +496,11 @@ case object TRetry                          extends TResult[Nothing]
 //       }
 //   }
 
-//   private[stm] object internal {
-
-//     @volatile var pendingQueue: Queue[Txn] = Queue.empty
-
-//     case class TLog(val map: MMap[TxId, TLogEntry]) {
-//       def apply(id: TxId): TLogEntry = map(id)
-
-//       def values: Iterable[TLogEntry] = map.values
-
-//       def contains(id: TxId): Boolean = map.contains(id)
-
-//       def +=(pair: (TxId, TLogEntry)) = map += pair
-
-//       //Returns a callback to revert the log to the state at the
-//       //point when snapshot was invoked
-//       def snapshot: () => Unit = {
-//         val snapshot: Map[TxId, Any] = map.toMap.map {
-//           case (id, e) => id -> e.current
-//         }
-//         () =>
-//           for (pair <- map)
-//             if (snapshot contains (pair._1))
-//               //The entry was already modified at
-//               //some point in the transaction
-//               pair._2.unsafeSet(snapshot(pair._1))
-//             else
-//               //The entry was introduced in the attempted
-//               //part of the transaction that we are now
-//               //reverting so we reset to the initial
-//               //value.
-//               //We don't want to remove it from the map
-//               //as we still want to add the currently
-//               //executing transaction to the set of
-//               //pending transactions for this tvar if
-//               //the whole transaction fails.
-//               pair._2.reset()
-//       }
-
-//     }
-
-//     object TLog {
-//       def empty: TLog = TLog(MMap[TxId, TLogEntry]())
-//     }
-
-//     type Pending = () => Unit
-
-//     type TxId = Long
-
-//     case class Txn(id: TxId, pending: Pending, tvs: Set[ETVar])
-
-//     // Existential TVar
-//     abstract class ETVar {
-//       type Repr
-//       val tv: TVar[Repr]
-//     }
-
-//     object ETVar {
-//       def apply[A](t: TVar[A]): ETVar =
-//         new ETVar {
-//           override type Repr = A
-//           override val tv = t
-//         }
-//     }
-
-//     abstract class TLogEntry {
-//       type Repr
-//       var current: Repr
-//       val initial: Repr
-//       val tvar: TVar[Repr]
-
-//       def unsafeGet[A]: A = current.asInstanceOf[A]
-
-//       def unsafeSet[A](a: A): Unit = current = a.asInstanceOf[Repr]
-
-//       def commit(): Unit = tvar.value = current
-
-//       def reset(): Unit = current = initial
-
-//     }
-
-//     object TLogEntry {
-
-//       def apply[A](tvar0: TVar[A], current0: A): TLogEntry =
-//         new TLogEntry {
-//           override type Repr = A
-//           override var current: A    = current0
-//           override val initial: A    = tvar0.value
-//           override val tvar: TVar[A] = tvar0
-//         }
-
-//     }
-
 //     sealed trait TResult[+A]                    extends Product with Serializable
 //     final case class TSuccess[A](value: A)      extends TResult[A]
 //     final case class TFailure(error: Throwable) extends TResult[Nothing]
 //     case object TRetry                          extends TResult[Nothing]
 
-//     val IdGen = new AtomicLong()
 //   }
 
 // }
--- core/src/main/scala/io/github/timwspence/cats/stm/TVar.scala
+++ core/src/main/scala/io/github/timwspence/cats/stm/TVar.scala
@@ -10,9 +10,10 @@ import io.github.timwspence.cats.stm.STM.internal._
   *
   * Analagous to `cats.effect.concurrent.Ref`.
   */
-final class TVar[A] private[stm] (
+final class TVar[+A] private[stm] (
   private[stm] val id: Long,
-  @volatile private[stm] var value: A,
+  //Safe by construction
+  @volatile private[stm] var value: Any,
   private[stm] val pending: AtomicReference[Map[TxId, Txn]]
 ) {
 
@@ -20,50 +21,24 @@ final class TVar[A] private[stm] (
     * Get the current value as an
     * `STM` action.
     */
-  def get: STM[A] =
-    STM { log =>
-      val entry = getOrInsert(log)
-      TSuccess(entry.unsafeGet[A])
-    }
+  def get: STM[A] = Get(this)
 
   /**
     * Set the current value as an
     * `STM` action.
     */
-  def set(a: A): STM[Unit] =
-    STM { log =>
-      val entry = getOrInsert(log)
-      TSuccess(entry.unsafeSet(a))
-    }
+  def set[B >: A](b: B): STM[Unit] = modify(_ => b)
 
   /**
     * Modify the current value as an
     * `STM` action.
     */
-  def modify(f: A => A): STM[Unit] =
-    STM { log =>
-      val entry   = getOrInsert(log)
-      val updated = f(entry.unsafeGet[A])
-      TSuccess(entry.unsafeSet(updated))
-    }
-
-  private def getOrInsert(log: TLog): TLogEntry =
-    if (log.contains(id))
-      log(id)
-    else {
-      val entry = TLogEntry(this, value)
-      log += id -> entry
-      entry
-    }
+  def modify[B >: A](f: A => B): STM[Unit] = Modify(this, f)
 
 }
 
 object TVar {
 
-  def of[A](value: A): STM[TVar[A]] =
-    STM { _ =>
-      val id = IdGen.incrementAndGet
-      TSuccess(new TVar(id, value, new AtomicReference(Map())))
-    }
+  def of[A](value: A): STM[TVar[A]] = Pure(new TVar(IdGen.incrementAndGet(), value, new AtomicReference(Map())))
 
 }
